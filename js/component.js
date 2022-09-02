3e7c45888dc7dac9e37f19bbd1574bcdbb51f7d1
Catalina Castillo <105820476+CatalinaCastilloB@users.noreply.github.com>
2022-09-02 15:51:11 -0400

    prueba
    Signed-off-by: Catalina Castillo <105820476+CatalinaCastilloB@users.noreply.github.com>

--- js/component.js
+++ js/component.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    $("#header").load("https://github.com/CatalinaCastilloB/paginaArte/component/header.html");
-    $("#footer").load("https://github.com/CatalinaCastilloB/paginaArte/component/footer.html");
+    $("#header").load("../paginaArte/component/header.html");
+    $("#footer").load("../paginaArte/component/footer.html");
 });
