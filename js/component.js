2191f03c779e3a69d2b61e2eec0471bb6f948997
catalinaCastilloB <ccastillobelmar@gmail.com>
2022-09-03 13:39:52 -0400

    cambios varios 3

--- js/component.js
+++ js/component.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    $("#header").load("../component/header.html");
-    $("#footer").load("../component/footer.html");
+    $("#header").load("./component(header.html");
+    $("#footer").load("./component/footer.html");
 });
